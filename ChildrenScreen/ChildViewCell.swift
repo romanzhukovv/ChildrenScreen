65d24299dd83a28b28098094d06286e6c599d0a2
romanzhukovv <zhukov.roman89@gmail.com>
2022-03-08 14:06:53 +0300

    reworked UI

---
+++ ChildrenScreen/ChildViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChildViewCell.swift
+//  ChildrenScreen
+//
+//  Created by Roman Zhukov on 08.03.2022.
+//
+
+import UIKit
+
+class ChildViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ChildrenScreen/ChildrenViewController.swift
+++ ChildrenScreen/ChildrenViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ChildrenViewController: UIViewController {
-
+    @IBOutlet var childrenTableView: UITableView!
+    @IBOutlet var addChildButton: UIButton!
+    @IBOutlet var clearInfoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
-
+    @IBAction func addChildButtonAction() {
+        
+    }
+    @IBAction func clearInfoButtonAction() {
+        
+    }
+    
 }
 
+extension ChildrenViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = childrenTableView.dequeueReusableCell(withIdentifier: "childCell") as! ChildViewCell
+        return cell
+    }
+}
+
+extension ChildrenViewController {
+    private func setupUI() {
+        addChildButton.layer.cornerRadius = 20
+        addChildButton.layer.borderWidth = 2
+        addChildButton.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        clearInfoButton.layer.cornerRadius = 20
+        clearInfoButton.layer.borderWidth = 2
+        clearInfoButton.layer.borderColor = UIColor.systemRed.cgColor
+    }
+    
+}
