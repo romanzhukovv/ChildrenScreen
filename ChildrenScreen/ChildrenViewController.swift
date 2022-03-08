efdfdeb4237db50eedf58323205b288845d4df66
romanzhukovv <zhukov.roman89@gmail.com>
2022-03-08 18:54:00 +0300

    finished project

--- ChildrenScreen/ChildrenViewController.swift
+++ ChildrenScreen/ChildrenViewController.swift
@@ -14,41 +14,41 @@ class ChildrenViewController: UIViewController {
     @IBOutlet var parentName: UITextField!
     @IBOutlet var parentAge: UITextField!
     
-    
-    
-    
-    private var childCount = 0
+    private var childrenList: [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            super.touchesBegan(touches, with: event)
+            view.endEditing(true)
+    }
 
     @IBAction func addChildButtonAction() {
-        if childCount != 5 {
-            childCount += 1
-            childrenTableView.reloadData()
-        } else {
-            addChildButton.isHidden = true
-        }
+        addChild()
     }
+    
+    @IBAction func deleteChildButtonAction(_ sender: UIButton) {
+        deleteChild(sender: sender)
+    }
+    
+    
     @IBAction func clearInfoButtonAction() {
-        childCount = 0
-        childrenTableView.reloadData()
-        parentName.text = ""
-        parentAge.text = ""
-        addChildButton.isHidden = false
+        showActionSheet()
     }
     
 }
 
 extension ChildrenViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        childCount
+        childrenList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = childrenTableView.dequeueReusableCell(withIdentifier: "childCell") as! ChildViewCell
+        cell.selectionStyle = .none
         return cell
     }
     
@@ -68,4 +68,48 @@ extension ChildrenViewController {
         clearInfoButton.layer.borderColor = UIColor.systemRed.cgColor
     }
     
+    private func addChild() {
+        if childrenList.count < 5 {
+            childrenList.insert(1, at: 0)
+
+            childrenTableView.beginUpdates()
+            childrenTableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
+            childrenTableView.endUpdates()
+            
+            if childrenList.count == 5 {
+                addChildButton.isHidden = true
+            }
+        }
+    }
+    
+    private func deleteChild(sender: UIButton) {
+        let point = sender.convert(CGPoint.zero, to: childrenTableView)
+        guard let indexPath = childrenTableView.indexPathForRow(at: point) else { return }
+        childrenList.remove(at: indexPath.row)
+        childrenTableView.beginUpdates()
+        childrenTableView.deleteRows(at: [IndexPath(row: indexPath.row, section: 0)], with: .left)
+        childrenTableView.endUpdates()
+        
+        if childrenList.count < 5 {
+            addChildButton.isHidden = false
+        }
+    }
+    
+    private func showActionSheet() {
+        let actionSheetController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let clearInfoAction = UIAlertAction(title: "Сбросить данные", style: .destructive) { _ in
+            self.childrenList = []
+            self.childrenTableView.reloadData()
+            self.parentName.text = ""
+            self.parentAge.text = ""
+            self.addChildButton.isHidden = false
+        }
+        
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
+        
+        actionSheetController.addAction(clearInfoAction)
+        actionSheetController.addAction(cancelAction)
+        
+        present(actionSheetController, animated: true, completion: nil)
+    }
 }
